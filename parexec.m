80b01231c7d3e048affef72ea8f6b34d66359b68
Dung Truong <dt.young112@gmail.com>
2022-08-23 16:58:06 -0400

    check file open error

--- parexec.m
+++ parexec.m
@@ -1,7 +1,7 @@
 function ALLEEG = parexec(data, fun, logdir, varargin)
     options = varargin;
     optionsText = argArray2Str(options);
-    fid = fopen(sprintf('%s/%s_params',logdir,fun),'w');
+    fid = openfile(sprintf('%s/%s_params',logdir,fun),'w');
     fprintf(fid, optionsText);
     fclose(fid);
     ALLEEG = data;
@@ -9,17 +9,17 @@ function ALLEEG = parexec(data, fun, logdir, varargin)
     parfor i=1:numel(data)
        EEG = data(i);
        try
-           fid = fopen(sprintf('%s/stdout', logdir),'a');
+           fid = openfile(sprintf('%s/stdout', logdir),'a');
            fprintf(fid, 'Evaluating %s for subject %s\n', fun, EEG.filename);
            fclose(fid);
            EEGout = feval(fun, EEG, options{:}); % script use EEG, which exists in current scope
            ALLEEG(i) = EEGout;
        catch ME
-           fid = fopen(sprintf('%s/stderr', logdir),'a');
+           fid = openfile(sprintf('%s/stderr', logdir),'a');
            fprintf(fid, 'Error evaluating %s at file %s\n', fun, fullfile(EEG.filepath, EEG.filename));
            fclose(fid);
            
-           fid = fopen(sprintf('%s/subject_%s.err',logdir, EEG.filename),'a');
+           fid = openfile(sprintf('%s/subject_%s.err',logdir, EEG.filename),'a');
            fprintf(fid, '\t%s:%s\n', ME.identifier, ME.message);
            fclose(fid);
        end
