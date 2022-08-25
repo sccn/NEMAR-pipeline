dbc0b152d2e122ccbf4799e42d998b0a2763c1ee
Dung Truong <dt.young112@gmail.com>
2022-08-25 07:16:16 -0700

    change the way default log dir is created

---
+++ create_and_submit_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+dsnumber="$1"
+path="/expanse/projects/nemar/openneuro/processed/logs/$1"
+mkdir $path
+
+sbatchfile="$path/$1sbatch"
+echo "#!/bin/bash" > $sbatchfile
+echo "#SBATCH -J $dsnumber" >> $sbatchfile
+echo "#SBATCH --partition=compute" >> $sbatchfile
+echo "#SBATCH --nodes=1" >> $sbatchfile
+echo "#SBATCH --mem=128G" >> $sbatchfile
+echo "#SBATCH -o $path/$dsnumber.out" >> $sbatchfile
+echo "#SBATCH -e $path/$dsnumber.err" >> $sbatchfile
+echo "#SBATCH -t 12:00:00" >> $sbatchfile
+echo "#SBATCH --account=csd403" >> $sbatchfile
+echo "#SBATCH --no-requeue" >> $sbatchfile
+echo "#SBATCH --ntasks-per-node=1" >> $sbatchfile
+
+echo "" >> $sbatchfile
+echo "cd /home/dtyoung/NEMAR-pipeline" >> $sbatchfile
+echo "module load matlab" >> $sbatchfile
+echo "matlab -nodisplay -r \"pipeline('$dsnumber');\"" >> $sbatchfile
+
+sbatch $sbatchfile
