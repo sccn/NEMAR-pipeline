a08155f39c837a1ff79496d46b9102455824404d
Dung Truong <dt.young112@gmail.com>
2022-12-16 19:58:59 -0800

    change error catching mechanism

--- bids_preprocess.m
+++ bids_preprocess.m
@@ -47,63 +47,80 @@ if opt.verbose
     ALLEEG(1).chanlocs(1)
 end
 
-% % remove non-ALLEEG channels (it is also possible to process ALLEEG data with non-ALLEEG data
-% get non-EEG channels
-% keep only EEG channels
-if isfield(ALLEEG(1).chanlocs, 'type')
-    types = {ALLEEG(1).chanlocs.type};
-    eeg_indices = strmatch('EEG', types)';
-    if size(eeg_indices,2) == 1 && size(eeg_indices,1) > 1
-        eeg_indices = eeg_indices';
-    end
-    if ~isempty(eeg_indices)
-        options = {'channel', eeg_indices};
-        ALLEEG = parexec(ALLEEG, 'pop_select', opt.logdir, options{:});
+options = {};
+try
+    % % remove non-ALLEEG channels (it is also possible to process ALLEEG data with non-ALLEEG data
+    % get non-EEG channels
+    % keep only EEG channels
+    rm_chan_types = {'AUDIO','MEG','EOG','ECG','EMG','EYEGAZE','GSR','HEOG','MISC','PPG','PUPIL','REF','RESP','SYSCLOCK','TEMP','TRIG','VEOG'};
+    if isfield(ALLEEG(1).chanlocs, 'type')
+        types = {ALLEEG(1).chanlocs.type};
+        ALLEEG = pop_select(ALLEEG, 'rmchantype', rm_chan_types);
+        eeg_indices = strmatch('EEG', types)';
+        if size(eeg_indices,2) == 1 && size(eeg_indices,1) > 1
+            eeg_indices = eeg_indices';
+        end
+        if ~isempty(eeg_indices)
+            ALLEEG = pop_select(ALLEEG, 'channel', eeg_indices);
+        else
+            warning("No EEG channel type detected (for first EEG file). Keeping all channels");
+        end
     else
-        warning("No EEG channel type detected (for first EEG file). Keeping all channels");
+        warning("Channel type not detected (for first EEG file)");
     end
-else
-    warning("Channel type not detected (for first EEG file)");
-end
-% ALLEEG = pop_select( ALLEEG,'nochannel',{'VEOG', 'Misc', 'ECG', 'M2'});
+    % ALLEEG = pop_select( ALLEEG,'nochannel',{'VEOG', 'Misc', 'ECG', 'M2'});
 
-if isfield(ALLEEG(1).chanlocs, 'theta')
-    thetas = [ALLEEG(1).chanlocs.theta];
-    if isempty(thetas)
-        warning("No channel locations detected (for first EEG file)");
+    if isfield(ALLEEG(1).chanlocs, 'theta')
+        thetas = [ALLEEG(1).chanlocs.theta];
+        if isempty(thetas)
+            warning("No channel locations detected (for first EEG file)");
+        end
     end
+
+    % % compute average reference
+    options = {[]};
+    ALLEEG = pop_reref( ALLEEG,options{:});
+
+    % clean data using the clean_rawdata plugin
+    options = {'FlatlineCriterion',5,'ChannelCriterion',0.85, ...
+        'LineNoiseCriterion',4,'Highpass',[0.25 0.75] ,'BurstCriterion',20, ...
+        'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian', ...
+        'WindowCriterionTolerances',[-Inf 7] ,'fusechanrej',1};
+    % ALLEEG = parexec(ALLEEG, 'pop_clean_rawdata', opt.logdir, options{:});
+    ALLEEG = pop_clean_rawdata( ALLEEG, options{:});
+    save_datasets(ALLEEG)
+
+    % recompute average reference interpolating missing channels (and removing
+    % them again after average reference - STUDY functions handle them automatically)
+    options = {[], 'interpchan', []}
+    ALLEEG = pop_reref( ALLEEG,options{:});
+    save_datasets(ALLEEG)
+
+    % run ICA reducing the dimention by 1 to account for average reference 
+    options = {'icatype','runica','concatcond','on','options',{'pca',-1}};
+    % ALLEEG = parexec(ALLEEG, 'pop_runica', opt.logdir, options{:});
+    ALLEEG = pop_runica(ALLEEG, options{:});
+    save_datasets(ALLEEG)
+
+    % % run ICLabel and flag artifactual components
+    options = {'default'};
+    ALLEEG = pop_iclabel(ALLEEG, options{:});
+    options = {[NaN NaN;0.9 1;0.9 1;NaN NaN;NaN NaN;NaN NaN;NaN NaN]};
+    ALLEEG = pop_icflag( ALLEEG, options{:});
+    save_datasets(ALLEEG)
+
+    fid = fopen(fullfile(fileparts(opt.logdir), 'pipeline_status.tsv'), 'a');
+    fprintf(fid, '%s\t%d\n', dsnumber, 1);
+    fclose(fid);
+    % pop_editoptions( 'option_storedisk', 1); % only one dataset at a time
+catch ME
+    save(sprintf('%s/ALLEEG_%s_%s.mat', opt.logdir, ME.identifier, char(datetime('now','TimeZone','local','Format','d-MMM-y-HHmmss'))), 'ALLEEG');
+    save(sprintf('%s/options_%s_%s.mat', opt.logdir, ME.identifier, char(datetime('now','TimeZone','local','Format','d-MMM-y-HHmmss'))), 'options');
+    fid = fopen(fullfile(fileparts(opt.logdir), 'pipeline_status.tsv'), 'a');
+    fprintf(fid, '%s\t%d', dsnumber, 0);
+    fclose(fid);
+    error('%s\n%s',ME.identifier, ME.getReport());
 end
-
-% % compute average reference
-ALLEEG = pop_reref( ALLEEG,[]);
-
-% clean data using the clean_rawdata plugin
-options = {'FlatlineCriterion',5,'ChannelCriterion',0.85, ...
-    'LineNoiseCriterion',4,'Highpass',[0.25 0.75] ,'BurstCriterion',20, ...
-    'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian', ...
-    'WindowCriterionTolerances',[-Inf 7] ,'fusechanrej',1};
-ALLEEG = parexec(ALLEEG, 'pop_clean_rawdata', opt.logdir, options{:});
-% ALLEEG = pop_clean_rawdata( ALLEEG,);
-save_datasets(ALLEEG)
-
-% recompute average reference interpolating missing channels (and removing
-% them again after average reference - STUDY functions handle them automatically)
-ALLEEG = pop_reref( ALLEEG,[],'interpchan',[]);
-save_datasets(ALLEEG)
-
-% run ICA reducing the dimention by 1 to account for average reference 
-options = {'icatype','runica','concatcond','on','options',{'pca',-1}};
-ALLEEG = parexec(ALLEEG, 'pop_runica', opt.logdir, options{:});
-% ALLEEG = pop_runica(ALLEEG,);
-save_datasets(ALLEEG)
-
-% % run ICLabel and flag artifactual components
-ALLEEG = pop_iclabel(ALLEEG, 'default');
-ALLEEG = pop_icflag( ALLEEG,[NaN NaN;0.9 1;0.9 1;NaN NaN;NaN NaN;NaN NaN;NaN NaN]);
-save_datasets(ALLEEG)
-
-% pop_editoptions( 'option_storedisk', 1); % only one dataset at a time
-
 function save_datasets(ALLEEG)
     for i=1:numel(ALLEEG)
         pop_saveset(ALLEEG(i), 'filepath', ALLEEG(i).filepath, 'filename', ALLEEG(i).filename);
