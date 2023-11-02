9263ea96ec3290c1e132acebae51a9d238aecfb7
Dung Truong <dt.young112@gmail.com>
2023-11-02 11:55:34 -0700

    add freeze header

--- web/pipeline_status.php
+++ web/pipeline_status.php
@@ -174,7 +174,8 @@ $handler = 'http://' . $_SERVER['HTTP_HOST'] . $directory_self . 'update.manual.
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.js"></script>
+    <script src="https://cdn.datatables.net/v/dt/dt-1.13.6/fh-3.4.0/datatables.min.js"></script>
+    <!--<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.20/datatables.min.js"></script>-->
 
     <script>
         $(document).ready(function() {
@@ -190,6 +191,7 @@ $handler = 'http://' . $_SERVER['HTTP_HOST'] . $directory_self . 'update.manual.
 		    });
 
 		    $('table').DataTable( {
+			"fixedHeader": true,
 			"paging": false,
 			"order": [[ 0, "asc"]],
 			//"columnDefs": [{"orderable": false, "targets":[3,8]}]
